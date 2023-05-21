b2393a6b58f063693d11df0d93b75d0e6399fa91
Makuochukwu225 <officialrrye5@gmail.com>
2023-05-21 13:57:27 +0100

    ...

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.set('debug', true);
 
 
-const connection = mongoose.createConnection(`mongodb+srv://officialrrye5:Q7gBJhDT0nMAGE0f@cluster0.on6hvbg.mongodb.net/?retryWrites=true&w=majority`).on('open', () => { console.log("MongoDB Connected"); }).on('error', () => {
+const connection = mongoose.createConnection(`mongodb+srv://officialrrye5:Q7gBJhDT0nMAGE0f@cluster0.on6hvbg.mongodb.net/todo`).on('open', () => { console.log("MongoDB Connected"); }).on('error', () => {
     console.log("MongoDB Connection error");
 });
 
